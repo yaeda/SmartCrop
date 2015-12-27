d2e27292ebf7e3647c4e41d26d91bd9c1e293863
Takeshi YAEDA <t.yaeda@gmail.com>
2015-12-27 22:10:13 +0900

    Add more aspect test

--- smartcrop/src/androidTest/java/com/kazeor/android/smartcrop/OptionTest.java
+++ smartcrop/src/androidTest/java/com/kazeor/android/smartcrop/OptionTest.java
@@ -1,55 +1,65 @@
 package com.kazeor.android.smartcrop;
 
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.test.InstrumentationTestCase;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.InputStream;
 
 public class OptionTest extends InstrumentationTestCase {
 
+    final float ASSERT_FLOAT_DELTA = 0.00001f;
     SmartCrop smartcrop = null;
+    Bitmap bitmap = null;
 
     private Context getApplicationContext() {
         return getInstrumentation().getTargetContext().getApplicationContext();
     }
 
-    private Resources getResoutce() {
+    private Resources getResources() {
         return getApplicationContext().getResources();
     }
 
     public void setUp() throws Exception {
         super.setUp();
         smartcrop = new SmartCrop();
+        TestSet testSet = AssetUtil.loadTestSet(getResources(), "test_monkey.json");
+        bitmap = AssetUtil.loadTestBitmap(getResources(), testSet.filename);
     }
 
     public void tearDown() throws Exception {
+        bitmap.recycle();
     }
 
     // crop should be something sane
-    public void testAspectOption() throws Exception {
-        AssetManager assetManager = getResoutce().getAssets();
-        InputStream isJson = assetManager.open("test_monkey.json");
-        TestSet testSet = new ObjectMapper().readValue(isJson, TestSet.class);
-
-        InputStream isImage = assetManager.open(testSet.filename);
-        Bitmap bitmap = BitmapFactory.decodeStream(isImage);
+    public void testAspectOption1by1() throws Exception {
         SmartCrop.Result result = smartcrop.crop(bitmap, 1);
 
         assertEquals(
                 bitmap.getWidth() * result.topCrop.width,
                 bitmap.getHeight() * result.topCrop.height
         );
+    }
 
-        // finalize
-        isJson.close();
-        isImage.close();
-        bitmap.recycle();
+    public void testAspectOption16by9() throws Exception {
+        float aspect = 16f / 9f;
+        SmartCrop.Result result = smartcrop.crop(bitmap, aspect);
+
+        assertEquals(
+                aspect,
+                (bitmap.getWidth() * result.topCrop.width) / (bitmap.getHeight() * result.topCrop.height),
+                ASSERT_FLOAT_DELTA
+        );
+    }
+
+    public void testAspectOption9by16() throws Exception {
+        float aspect = 9f / 16f;
+        SmartCrop.Result result = smartcrop.crop(bitmap, aspect);
+
+        assertEquals(
+                aspect,
+                (bitmap.getWidth() * result.topCrop.width) / (bitmap.getHeight() * result.topCrop.height),
+                ASSERT_FLOAT_DELTA
+        );
     }
 
 }
